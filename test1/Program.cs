11c688d5922c92863e69944933f51272d1766468
lisa <lisma_li@mail.ru>
2022-09-10 13:44:02 +0300

    add solution task

---
+++ test1/Program.cs
@@ -0,0 +1,18 @@
+﻿//напишите программу, которая из имеющегося массива строк
+//  формирует массив из строк, длина которых меньше либо равна 3
+
+    string[] stringArray = {"hello", "2", "world", ":-)"};
+    string[] newArray = {};
+
+    Array.Resize(ref newArray, stringArray.Length);
+
+    for (int i=0; i < stringArray.Length; i++) {
+      if (stringArray[i].Length <= 3) {
+        newArray[i] = stringArray[i];
+      }
+    }
+
+    for (int i=0; i < newArray.Length; i++) {
+      Console.WriteLine(newArray[i]);
+    }
+    
